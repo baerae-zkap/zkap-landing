c9d1df6e9f3490d903a5cfb4ca20f3d516900756
jaden <jaden@jadenui-MacBookPro.local>
2026-03-30 13:01:16 +0900

    feat: assemble all 9 sections into full landing page

--- src/app/[locale]/page.tsx
+++ src/app/[locale]/page.tsx
@@ -1,8 +1,14 @@
 import Header from "@/components/Header";
+import Footer from "@/components/Footer";
 import HeroSection from "@/components/sections/HeroSection";
 import RiskWarningSection from "@/components/sections/RiskWarningSection";
 import SelfDiagnosisSection from "@/components/sections/SelfDiagnosisSection";
 import ReliefSection from "@/components/sections/ReliefSection";
+import ExchangeAssetSection from "@/components/sections/ExchangeAssetSection";
+import ProcessSection from "@/components/sections/ProcessSection";
+import TrustSection from "@/components/sections/TrustSection";
+import FAQSection from "@/components/sections/FAQSection";
+import FinalCTASection from "@/components/sections/FinalCTASection";
 
 export default function Home() {
   return (
@@ -13,7 +19,13 @@ export default function Home() {
         <RiskWarningSection />
         <SelfDiagnosisSection />
         <ReliefSection />
+        <ExchangeAssetSection />
+        <ProcessSection />
+        <TrustSection />
+        <FAQSection />
+        <FinalCTASection />
       </main>
+      <Footer />
     </>
   );
 }
